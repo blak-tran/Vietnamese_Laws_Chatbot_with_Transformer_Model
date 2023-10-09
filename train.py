188fd5dda22579efd3725e3c9b335a18e0f644ee
blak-tran <ceilingdat@gmail.com>
2023-10-09 14:26:38 +0000

    update train arg

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import logging
 import pandas as pd
 import os
 import tensorflow as tf
-from preprocess_data import preprocessing
+from preprocess_data import data_processing
 from models import transformer
 from loss import CustomSchedule, loss_function, accuracy
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
@@ -13,7 +13,7 @@ logging.basicConfig(filename='training.log', level=logging.INFO, format='%(ascti
 def parse_args():
     parser = argparse.ArgumentParser(description='Training script for chatbot model.')
     parser.add_argument('--epochs', type=int, default=10000, help='Number of training epochs')
-    parser.add_argument('--batch_size', type=int, default=128, help='Batch size for training')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training')
     parser.add_argument('--num_layers', type=int, default=2, help='Number of transformer layers')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for the optimizer')
     parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate')
@@ -21,6 +21,9 @@ def parse_args():
     parser.add_argument('--save_dir', type=str, default='./checkpoints', help='Directory to save model checkpoints')
     parser.add_argument('--log_dir', type=str, default='./logs', help='Directory to save training logs')
     parser.add_argument('--data_file', type=str, default='input/vietnamese-chatbot/d liu chatbot question-answer short style.csv', help='Path to CSV file containing training data')
+    parser.add_argument('--save_frequency', type=int, default=500, help="Model checkpoint will be saved every epochs th")
+    parser.add_argument('--checkpoint_path', type=str, default=None, help='Path to the pre-trained model checkpoint')
+    parser.add_argument('--word2vec', action='store_true', default=True, help='Path to the pre-trained model checkpoint')
     return parser.parse_args()
 
 def setup_tpu():
@@ -67,6 +70,11 @@ def train_model(args, encoder_input_data, decoder_input_data, decoder_output_dat
         save_weights_only=True,
         verbose=1
     )
+    
+    if args.checkpoint_path is not None:
+        # Load the checkpoint weights
+        model.load_weights(args.checkpoint_path)
+        print(f"Loaded checkpoint weights from {args.checkpoint_path}")
 
     # Training the model with TensorBoard and ModelCheckpoint callbacks
     history = model.fit([encoder_input_data, decoder_input_data], decoder_output_data, batch_size=args.batch_size, epochs=args.epochs, callbacks=[tensorboard_callback, checkpoint_callback])
@@ -77,7 +85,9 @@ def main():
     args = parse_args()
     strategy = setup_tpu()
     dataframe = pd.read_csv(args.data_file)
+    
     encoder_input_data, decoder_input_data, decoder_output_data, maxlen_answers, embedding_matrix, maxlen_questions, VOCAB_SIZE, embeddings_dim = data_processing(dataframe=dataframe)
+    
     train_model(args, encoder_input_data, decoder_input_data, decoder_output_data, maxlen_answers, embedding_matrix, maxlen_questions, VOCAB_SIZE, embeddings_dim)
 
 if __name__ == "__main__":
